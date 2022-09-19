eaae9e629d6304dd39b7b86375d57128c6d5e761
RIP-Network <93273837+RIP-Network@users.noreply.github.com>
2022-09-19 21:38:48 +0200

    Update email.sh

--- email.sh
+++ email.sh
@@ -1,4 +1,36 @@
 #!bin/bash
+clear
+
+if [ "$(id -u)" != "0" ] > /dev/null 2>&1; then
+echo -e '\n$red[x] Este script necesita permisos root.' 1>&2
+exit
+fi
+
+ping -c 1 google.com > /dev/null 2>&1
+if [[ "$?" == 0 ]]; then
+echo ""
+echo -e "$green[✔][Internet Connection]............[ OK ]"
+sleep 1.5
+else
+echo ""
+echo -e $red"[!][Internet Connection].........[ NOT FOUND ]"
+echo ""
+exit
+fi
+
+which xterm > /dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+echo ""
+echo -e "$green[✔][Xterm]..........................[ OK ]"
+sleep 1.5
+else
+echo ""
+echo -e "$red[x][Xterm].......................[ NOT FOUND ]"
+sleep 1.5
+echo -e "$yellow[!][Installing Xterm...]"
+sudo apt-get install -y xterm > /dev/null
+fi
+
 
 open https://www.youtube.com/channel/UCwYRhxoXiqlDpBp-HjjzQ7w
 
@@ -9,14 +41,16 @@ echo "█████╗  ██╔████╔██║███████
 echo "██╔══╝  ██║╚██╔╝██║██╔══██║██║██║         ██║     ██╔══██║██╔══╝  ██║     ██╔═██╗ ██╔══╝  ██╔══██╗"
 echo "███████╗██║ ╚═╝ ██║██║  ██║██║███████╗    ╚██████╗██║  ██║███████╗╚██████╗██║  ██╗███████╗██║  ██║"
 echo "╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝     ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝"
-echo "                    version 1.0                   creado por RIP-Network "
+echo "                    version 2.0                   creado por RIP-Network "
 echo
 echo 
 echo "https://emailable.com/api/ para conseguir la API "
 echo "Mi github https://github.com/RIP-Network"
 echo
 read -p "[*] Escribe el correo electronico: " email
-echo "Espere por favor"
+echo "dirigiendose a la pagina."
+sleep 2
+open https://emailable.com/api/
 read -p "[*] Escriba su API: " your_api_key
 sleep 5
 
